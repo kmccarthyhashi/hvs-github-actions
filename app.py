2e0887efdc548e066abfbe696fb0261a7a5a5e61
kmccarthyhashi <107080002+kmccarthyhashi@users.noreply.github.com>
2024-07-25 14:45:56 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+"""Cloud Foundry test"""
+from flask import Flask
+import os
+app = Flask(__name__)
+# On Bluemix, get the port number from the environment variable VCAP_APP_PORT
+# When running this app on the local machine, default the port to 8080
+port = int(os.getenv('VCAP_APP_PORT', 8080))
+secret = os.getenv('SECRET')
+@app.route('/')
+def hello_world():
+    return 'Hello World! The secret is:' +  str(secret)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
